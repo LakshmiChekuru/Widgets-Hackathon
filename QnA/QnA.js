fa66118d01c5b1329c0402081afde641143c4b77
Lakshmi <lakshmi.chaitanya@asg.com>
2018-11-09 11:07:58 +0530

    Ajax call

--- QnA/QnA.js
+++ QnA/QnA.js
@@ -1,4 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+// internal libs
+import '../jslibs/ajax/ajax.js';
 
 /**
  * `demo-widget`
@@ -16,12 +18,30 @@ class QnA extends PolymerElement {
           display: block;
         }
       </style>
-      <h2>Hello [[checkValue]] [[prop1]]!</h2>
-      <input type="text"></input>
+      <h2>May I help you?</h2>
+      <div id="div1">
+      Q:<input type="text" id="Q1" value=[[prop1]]></input> <input type="button" on-click="_buttonClick" value="Send"></input>
+      <div id="A1"></div>      
+      </div>
+
+
+
+      <div id="template" hidden$="{{hide}}">
+      Q:<input type="text" id="Q1" value=[[prop1]]></input> <input type="button" on-click="_buttonClick" value="Send"></input>
+      <div></div>      
+      </div>
     `;
   }
   static get properties() {
     return {
+      hide: {
+        type: Boolean,
+        value: true // init the value to true so it will be hidden on page load
+    },
+      count: {
+        type: Number,
+        value:1
+      },
       prop1: {
         type: String,
         value: 'qna-widget',
@@ -29,8 +49,112 @@ class QnA extends PolymerElement {
       checkValue:{
         type: String,
         value: 'check',
+      },
+      answer:{
+        type:String,
+        value:'default'
       }
+
     };
+  }
+  _buttonClick(){    
+    this.answer="abcd efgh ijk lmn opq rst uvw";   
+    const container=this.shadowRoot.querySelector('#' + 'A'+this.count);
+    if(container)
+    {   
+      this.answer= this._getAnswer(this.shadowRoot.querySelector('#' + 'Q'+this.count).value);
+      container.innerHTML=this.answer;    
+    } 
+    
+    var cloned=this._cloneTemplate();
+    console.log("cloned count=" +cloned.childNodes.count);
+    const lastDIV=this.shadowRoot.querySelector('#' + 'div'+this.count);
+    lastDIV.parentNode.appendChild(cloned);
+  }
+
+
+  constructor() {
+    super();
+    this._init();
+  }
+
+  _init() {
+    this._apiUrl = 'https://workspaceqna.azurewebsites.net/qnamaker/knowledgebases/e4099bc6-9135-47d6-bb73-8a348b302ab1/generateAnswer/';
+  }
+  
+  _sendRequest(url, urlType, data, header) {
+    let options = {
+      url: url,
+      type: typeof urlType !== 'undefined' ? urlType : 'GET',
+      async: false,
+      headers: header,
+      crossDomain: true
+    };
+
+    if (data) {
+      options.data = JSON.stringify(data);
+    }
+
+    let self = this;
+       
+    return new Promise((resolve, reject) => {
+      atomic.ajax(options)
+        .success((response, xhr) => {
+          resolve(response);
+        })
+        .error((err, xhr) => {
+          // Define Error
+        })
+        .always(() => {
+          // Define Default operation 
+        });
+    });
+  }
+
+  _getAnswer(question) {
+    var url = this._apiUrl;
+    var urlType = "POST";
+    var header = {  'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'EndpointKey 59b657f7-5084-40be-ab5a-6dfba8fe531e'};
+    var data = {'question' : 'which bank supported ?'};
+
+    this._sendRequest(url, urlType, data,header)
+      .then((response) => {
+       // Define response
+       var x="abc";
+      }, function (error) { return; });
+  }
+
+  _cloneTemplate(){
+    const container = this.shadowRoot.querySelector('#template');
+    var ret = container.cloneNode();
+    var lightDom;
+    var childNodes;
+    var nextCount=this.count+1;
+    //ret.attributes.setNamedItem("id").value="div"+nextCount;
+    console.log(ret.attributes.getNamedItem("id").value);
+    //lightDom = Polymer.dom(ret);
+    childNodes = container.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {      
+      ret.appendChild(childNodes[i].cloneNode());
+    }    
+    return ret;
+  }
+
+  
+
+
+  _createAnswerData(htmlId) {
+    const container = this.shadowRoot.querySelector('#' + htmlId);
+    if (container) {
+      
+        let option = document.createElement('option');
+        option.textContent = item;
+        option.value = item;
+        option.selected = optionRangeSelected ? this._setSelected(this.opportunityrange, item) : this._setSelected(this.opportunityfilter, item);
+        container.appendChild(option);      
+    }
   }
 }
 
